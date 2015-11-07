1cfa1effd0e3cdc5b8f127805a3e078d07717214
Achie <krishna.achanta@gmail.com>
2015-11-07 10:00:44 -0800

    Added Title toolbar

--- app/src/main/java/com/zipcode/BaseActivity.java
+++ app/src/main/java/com/zipcode/BaseActivity.java
@@ -2,6 +2,9 @@ package com.zipcode;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.widget.TextView;
 
 import com.zipcode.api.ZipdroidApi;
 
@@ -10,6 +13,9 @@ import javax.inject.Inject;
 import butterknife.ButterKnife;
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+    protected Toolbar mTitleToolbar;
+    protected TextView mTitle;
 
     @Inject
     public ZipdroidApi mApi;
@@ -25,5 +31,40 @@ public abstract class BaseActivity extends AppCompatActivity {
             setContentView(getLayoutResId());
             ButterKnife.inject(this);
         }
+
+        initToolbar();
+    }
+
+    protected void initToolbar() {
+        if (usesDefaultToolbar()) {
+            mTitleToolbar = (Toolbar) findViewById(R.id.toolbar_activity_title);
+            setSupportActionBar(mTitleToolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(showUpNavigation());
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            mTitle = (TextView) findViewById(R.id.toolbar_title);
+            if (!TextUtils.isEmpty(getTitle())) {
+                mTitle.setText(getTitle());
+            }
+        }
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        super.setTitle(title);
+        mTitle.setText(title);
+    }
+
+    @Override
+    public void setTitle(int titleId) {
+        super.setTitle(titleId);
+        mTitle.setText(titleId);
+    }
+
+    protected boolean usesDefaultToolbar() {
+        return true;
+    }
+
+    protected boolean showUpNavigation() {
+        return true;
     }
 }
--- app/src/main/java/com/zipcode/MainActivity.java
+++ app/src/main/java/com/zipcode/MainActivity.java
@@ -4,14 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.zipcode.listing.ListingActivity;
 import com.zipcode.model.response.ListingsResponse;
-import android.widget.Button;
 
 import butterknife.OnClick;
 import retrofit.Callback;
@@ -25,8 +24,6 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
